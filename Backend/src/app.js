ca1a3a1d6c140a41b7b94136f5f05a329a42cd44
Vinod Kesharwani <Vinodkesharwanicktd@gmail.com>
2025-09-14 18:15:55 +0530

    Ready for Deployement

---
+++ Backend/src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose, { set } from "mongoose";
+
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+import cors from "cors";
+import connectToSocket from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ extended: true, limit: "40kb" }));
+app.use("/api/v1/users", userRoutes);
+
+
+const start = async () => {
+  app.set("mongo_user");
+  const connectionDb = await mongoose.connect(
+  
+    "mongodb+srv://VinodGuru95:DaminiShukla95@apnavideocall.jqyvbke.mongodb.net/"
+
+    
+  );
+
+  console.log(`MONGO CONNECTED DB HOST:${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log(`Server  is running on the port ${app.get("port")}`);
+  });
+};
+start();
---
+++ frontend/src/environment.js
@@ -0,0 +1,9 @@
+
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://apna-video-call-4-ikgf.onrender.com" :
+
+    "http://localhost:8000"
+
+
+export default server;
